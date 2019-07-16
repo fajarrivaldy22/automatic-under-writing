edb19b973e1725d59307941df863b1be0fa6a124
fajarrivaldy22 <33659035+fajarrivaldy22@users.noreply.github.com>
2019-07-16 23:20:22 +0700

    16 july

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('predicts/<int:umur>/<int:sa>/<int:penyakit>/',views.predict_user,name="predict")
+    path('predicts/<int:umur>/<int:sa>/<int:penyakit>/',views.predict_user,name="predict"),
+    path('predicts_urine/',views.predict_urine,name="predict_urine"),
+    path('predicts_blood/',views.predict_blood,name="preict_blood")
+    #<str:bili>/<str:uro>/<str:keton>/<str:protein>/<str:nitrit>/<str:lekosit>/<str:glu>/<str:ph>/
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,9 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
-# Create your views here.
+from sklearn import preprocessing
+import math
+
 from django.http import HttpResponse
 
 
@@ -43,6 +45,101 @@ def predict_user(request,umur,sa,penyakit):
 
     predict = neigh.predict([[umu,up,penyaki]])
     proba = neigh.predict_proba([[umu,up,penyaki]])
+
+    return HttpResponse(predict)
+
+def predict_urine(request):
+    csv = open(os.path.dirname(os.path.realpath(__file__))+'//dataframe_urine.csv','r')
+    data= pd.read_csv(csv)
+    
+    label = data.iloc[:,-1].values
+    data.head
+    data.drop(['eritrosit'],inplace=True,axis=1)
+
+    categorical_encoder = []
+    for i in range(len(data.columns)-1):
+        le = preprocessing.LabelEncoder()
+        categorical_encoder.append(le)
+        categorical_encoder[i].fit(data.iloc[:,i])
+        data.iloc[:,i] = categorical_encoder[i].transform(data.iloc[:,i])
+    kelas = data.iloc[:,0:len(data.columns)-1].values
+    neigh = KNeighborsClassifier(n_neighbors=math.ceil(math.sqrt(len(data))))
+
+    neigh.fit(kelas,label)
+
+    bilirubin = "negatif"
+    urobilin = "12"
+    keton = "3"
+    protein = "150"
+    nirit = "pos"
+    leukosit = "500"
+    gluskosa = "250"
+    ph = ">8"
+
+    bahan_uji = []
+    bahan_uji.append([bilirubin])
+    bahan_uji.append([urobilin])
+    bahan_uji.append([keton])
+    bahan_uji.append([protein])
+    bahan_uji.append([nirit])
+    bahan_uji.append([leukosit])
+    bahan_uji.append([gluskosa])
+    bahan_uji.append([ph])
+
+    encoded_categorical = []
+    for i in range(len(categorical_encoder)):
+        j = categorical_encoder[i].transform(bahan_uji[i])
+        encoded_categorical.append(j[0])
+    data_result = np.array(encoded_categorical)
+    data_result = data_result.reshape(1, -1)
+
+    predict = neigh.predict(data_result)
+    probability = neigh.predict_proba(data_result)
+
+    return HttpResponse(predict)
+
+
+def predict_blood(request):
+    csv = open(os.path.dirname(os.path.realpath(__file__))+'//dataframe_darah.csv','r')
+    data = pd.read_csv(csv)
+    label = data.iloc[:,-1].values
+    data.head
+
+    categorical_encoder = []
+    for i in range(len(data.columns)-1):
+        le = preprocessing.LabelEncoder()
+        categorical_encoder.append(le)
+        categorical_encoder[i].fit(data.iloc[:,i])
+        data.iloc[:,i] = categorical_encoder[i].transform(data.iloc[:,i])
+    
+    kelas = data.iloc[:,0:len(data.columns)-1].values
+    
+    neigh = KNeighborsClassifier(n_neighbors=2)
+    neigh.fit(kelas,label)
+
+    hemogoblin = ">18,0"
+    hemaktorit = ">50"
+    leukosit = "<4500"
+    trombosit = "normal"
+    led = "<15"
+    eritorsit = "normal"
+
+    bahan_uji = []
+    bahan_uji.append([hemogoblin])
+    bahan_uji.append([hemaktorit])
+    bahan_uji.append([leukosit])
+    bahan_uji.append([trombosit])
+    bahan_uji.append([led])
+    bahan_uji.append([eritorsit])
+
+    encoded_categorical = []
+    for i in range(len(categorical_encoder)):
+        j = categorical_encoder[i].transform(bahan_uji[i])
+        encoded_categorical.append(j[0])
+    data_result = np.array(encoded_categorical)
+    data_result = data_result.reshape(1, -1)
+    
+    predict = neigh.predict(data_result)
 
     return HttpResponse(predict)
     
